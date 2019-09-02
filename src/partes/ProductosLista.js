cb8cf98f2dd2d62351100e5e208dabdb5b5eed85
Andrea Gimenez <agimenez@tarifar.com>
2019-09-02 13:06:29 -0300

    ajax request

--- src/partes/ProductosLista.js
+++ src/partes/ProductosLista.js
@@ -3,7 +3,7 @@ import React, {Component} from 'react'
 export class ProductosLista extends Component {
     render(){
         return(
-            <div className="listado-productos">{this.props.titulo}</div>
+            <div className="listado-productos">{this.props.title}</div>
         )
     }
 }
