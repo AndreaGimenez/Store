44c22b3ccfbde931f6605800a577ae01cd507f39
Andrea Gimenez <agimenez@tarifar.com>
2019-09-04 10:30:35 -0300

    se mueven las imagenes del banner

--- src/partes/banner.js
+++ src/partes/banner.js
@@ -35,20 +35,34 @@ export class Banner extends Component {
       if (response.status !== 200) throw Error(body.message);
       return body;
     }
-
-
+   
     render(){
       var loading = (<div id="posicion-spinner"><i className="fa fa-spinner fa-spin fa-10x"></i></div>);
       const { currentImageIndex, error, isLoaded, slides } = this.state;
+      const movement = (num) => {
+        if(num === -1){
+          if(currentImageIndex === 0){
+            this.setState({currentImageIndex: slides.length - 1})
+          } else {
+            this.setState({currentImageIndex: currentImageIndex + num})}
+        } else {
+          if(currentImageIndex === slides.length - 1){
+            this.setState({currentImageIndex: 0})
+          } else {
+            this.setState({currentImageIndex: currentImageIndex + num})}
+          } 
+        }
 
       return(this.state.isLoaded ?
 
-        <div className="slide">
+        <div className="slider">
 	        <Slide imagen={slides[currentImageIndex]}/>
-          <Arrow direction="right" glyph="&#9654;" move={()=>{this.setState({currentImageIndex: currentImageIndex + 1})}}/>
-          <Arrow  direction="left" glyph="&#9664;" move={()=>{this.setState({currentImageIndex: currentImageIndex - 1})}}/>
+          <Arrow direction="right" glyph="&#9654;" move={()=>{movement(1)}}/>
+          <Arrow  direction="left" glyph="&#9664;" move={()=>{movement(-1)}}/>
       </div>
         : loading)
 
     }
 }
+
+
